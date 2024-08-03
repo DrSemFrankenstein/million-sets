9808853599b8f31fa3adf2cd55d31ac6248da565
DrSemFrankenstein <DrSemFrankenstein>
2024-08-03 20:16:15 +0300

    repo name

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: 'million-sets',
+  base: '/million-sets/',  // Replace <REPO_NAME> with your actual repository name
   plugins: [
     react(),
     VitePWA({
