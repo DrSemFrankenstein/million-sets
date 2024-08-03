2e0915205cd98fc593a3f1cbfc192c657349fc47
DrSemFrankenstein <DrSemFrankenstein>
2024-08-03 20:27:15 +0300

    ..

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
-  base: '/million-sets/',  // Replace <REPO_NAME> with your actual repository name
+  base: '/million-sets/',  // Correct base path for GitHub Pages
   plugins: [
     react(),
     VitePWA({
